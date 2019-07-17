f63bb8aad124279754b83f6b5e1c0826d004d31a
nzbenji <benlittle53@gmail.com>
2019-07-17 19:16:22 +1200

    added routing

---
+++ src/components/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+
+import Header from './Header';
+
+import ForgotPassword from './Auth/ForgotPassword';
+import Login from './Auth/Login';
+
+import CreateLink from './Link/CreateLink';
+import SearchLinks from './Link/SearchLinks';
+import LinkList from './Link/LinkList';
+import LinkDetail from './Link/LinkDetail';
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <div className="app-container">
+                <Header />
+                <div className="route-container">
+                    <Switch>
+                        <Route exact path="/" render={() => <Redirect to="/new/1" />} />
+                        <Route path="/create" component={CreateLink}/>
+                        <Route path="/login" component={Login}/>
+                        <Route path="/forgot" component={ForgotPassword}/>
+                        <Route path="/search" component={SearchLinks}/>
+                        <Route path="/top" component={LinkList}/>
+                        <Route path="/new/:page" component={LinkList}/>
+                        <Route path="/link/:linkId" component={LinkDetail}/>
+                    </Switch>
+                </div>
+            </div>
+        
+    </BrowserRouter>
+    )
+    
+}
+export default App;
