363ebc35a51e49c26edd3e68ce68acf4c421e5db
nzbenji <benlittle53@gmail.com>
2019-07-17 20:46:25 +1200

    added login forms

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Login(props) {
-  return <div>Login</div>;
+const Login = props => {
+  return (
+    <div>
+      <h2 className="mv3">Create Account</h2>
+      <form className="flex flex-column">
+        <input 
+          type="text"
+          placeholder="name"
+          autoComplete="off"
+        />
+        <input 
+          type="email"
+          placeholder="email"
+          autoComplete="off"
+        />
+        <input 
+          type="password"
+          placeholder="password"
+        />
+        <div className="flex mv3">
+          <button
+            type="button"
+            className="button pointer mv2"
+
+          >Submit</button>
+          <button type="button" className="pointer button">
+            Already have an account?
+          </button>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default Login;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,30 @@
-
 import React from "react";
+import { withRouter, NavLink } from 'react-router-dom';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="flex">
+        <NavLink to="/" className="header-title">
+          Hacker News
+        </NavLink>
+        <NavLink to="/new" className="header-link">
+          New
+        </NavLink>
+        <div className="divider"> | </div>
+        <NavLink to="/new" className="header-link">
+          Top
+        </NavLink>
+        <div className="divider"> | </div>
+        <NavLink to="/search" className="header-link">Search</NavLink>
+        <div className="divider"> | </div>
+        <NavLink to="/create" className="header-link">Submit</NavLink>
+      </div>
+      <div className="flex">
+        <NavLink to="/login" className="header-link">Logins</NavLink>
+      </div>
+    </div>
+  )
 }
 
 export default Header;
