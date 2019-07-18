2606813fb6048860b9bcbd4f57e56d20c0625efb
nzbenji <benlittle53@gmail.com>
2019-07-18 20:43:13 +1200

    custom form validation hook

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,30 +1,50 @@
 import React, { useState } from "react";
+import useFormValidation from './useFormValidation'
+
+const INITIAL_STATE = {
+  name: '',
+  email: '',
+  password: ''
+}
 
 const Login = props => {
+  const { handleChange, handleSubmit, values } = useFormValidation(INITIAL_STATE);
   const [login, setLogin] = useState(true);
   return (
     <div>
       <h2 className="mv3">{login ? 'Login' : 'Create Account'}</h2>
-      <form className="flex flex-column">
-        {!login && <input 
-          type="text"
-          placeholder="name"
-          autoComplete="off"
-        />}
+      <form 
+        onSubmit={console.log('gfdgdf')}
+        className="flex flex-column">
+        {!login && (
+          <input 
+            onChange={handleChange}
+            value={values.name}
+            name="name"
+            type="text"
+            placeholder="name"
+            autoComplete="off"
+          />)}
         <input 
+          onChange={handleChange}
+          value={values.email}
           type="email"
+          name="email"
           placeholder="email"
           autoComplete="off"
         />
         <input 
+          onChange={handleChange}
+          value={values.password}
           type="password"
+          name="password"
           placeholder="password"
         />
         <div className="flex mv3">
           <button
             type="button"
             className="button pointer mv2"
-
+            onClick={handleSubmit}
           >Submit</button>
           <button type="button" className="pointer button"
             onClick={() => setLogin(prevLogin => !prevLogin)}
